cebe6bd60f2a626ca04c1b657a285eb3e7a57140
dddoGitHub <30666817+dddoGitHub@users.noreply.github.com>
2017-08-03 03:41:33 +0900

    Create pascalHelloWandbox.pas

---
+++ pascalHelloWandbox.pas
@@ -0,0 +1,9 @@
+// This file is a "Hello, world!" in Pascal language by Free Pascal for wandbox.
+
+program Hello(output);
+begin
+    writeln('Hello, Wandbox!')
+end.
+
+// Free Pascal reference:
+//   http://www.freepascal.org/
